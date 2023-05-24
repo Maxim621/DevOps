c9dc5ef2d683b27de4d98c29f6e31319f0c13925
ADM <parfemax@gmail.com>
2023-05-24 11:33:00 +0300

    DepartmentController modification

--- DevOps/Controllers/DepartmentController.cs
+++ DevOps/Controllers/DepartmentController.cs
@@ -30,5 +30,47 @@ namespace DevOps.Controllers
             return new JsonResult(dbList);
         }
 
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+
+            int LastDepartmentId = dbClient.GetDatabase("testdb").GetCollection<Department>("Department").AsQueryable().Count();
+            dep.DepartmentId = LastDepartmentId + 1;
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").InsertOne(dep);
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", dep.DepartmentId);
+
+            var update = Builders<Department>.Update.Set("DepartmentName", dep.DepartmentName);
+
+
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").UpdateOne(filter, update);
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", id);
+
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").DeleteOne(filter);
+
+            return new JsonResult("Deleted Successfully");
+        }
     }
 }
