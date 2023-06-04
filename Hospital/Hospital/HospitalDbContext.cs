4ce00de6c1ad77b40230a813a1d9e73a253eb710
ADM <parfemax@gmail.com>
2023-06-04 23:48:39 +0300

    Create HospitalDbContext

---
+++ Hospital/Hospital/HospitalDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Hospital.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
