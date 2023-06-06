0331d17ef95e365643bbd3993f96016fb55ff436
ADM <parfemax@gmail.com>
2023-06-06 15:21:01 +0300

    Created repositories

---
+++ Hospital/Hospital/Repositories/DoctorRepository.cs
@@ -0,0 +1,45 @@
+﻿using Hospital.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hospital.Repositories
+{
+    public class DoctorRepository
+    {
+        private readonly HospitalDbContext _context;
+
+        public DoctorRepository(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Doctor> GetAllDoctors()
+        {
+            return _context.Doctors.ToList();
+        }
+
+        public Doctor GetDoctorById(int id)
+        {
+            return _context.Doctors.FirstOrDefault(d => d.Id == id);
+        }
+
+        public void AddDoctor(Doctor doctor)
+        {
+            _context.Doctors.Add(doctor);
+            _context.SaveChanges();
+        }
+
+        public void UpdateDoctor(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            _context.SaveChanges();
+        }
+
+        public void DeleteDoctor(Doctor doctor)
+        {
+            _context.Doctors.Remove(doctor);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Hospital/Hospital/Repositories/PatientRepository.cs
@@ -0,0 +1,45 @@
+﻿using Hospital.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hospital.Repositories
+{
+    public class PatientRepository
+    {
+        private readonly HospitalDbContext _context;
+
+        public PatientRepository(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Patient> GetAllPatients()
+        {
+            return _context.Patients.ToList();
+        }
+
+        public Patient GetPatientById(int id)
+        {
+            return _context.Patients.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void AddPatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+            _context.SaveChanges();
+        }
+
+        public void UpdatePatient(Patient patient)
+        {
+            _context.Patients.Update(patient);
+            _context.SaveChanges();
+        }
+
+        public void DeletePatient(Patient patient)
+        {
+            _context.Patients.Remove(patient);
+            _context.SaveChanges();
+        }
+    }
+}
